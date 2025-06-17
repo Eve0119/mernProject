ac84f89195509b558947d5fbb79aae2ac087528f
Eve0119 <gerictorilla@gmail.com>
2025-06-17 13:59:39 +0800

    .env file

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 export const connectDB = async () => {
     try {
-        await mongoose.connect("mongodb+srv://mytesting2233:V7k8HPoVIlOD0ne9@cluster0.r4davnn.mongodb.net/notes_db?retryWrites=true&w=majority&appName=Cluster0")
+        await mongoose.connect(process.env.MONGO_URI)
         console.log("MongoDB connected successfully");
     }catch (error) {
         console.error("MongoDB connection failed:", error.message);
