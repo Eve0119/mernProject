02c2efd9de2f4c9230d4b565dbf3a0538dc42b7d
Eve0119 <gerictorilla@gmail.com>
2025-06-17 13:37:39 +0800

    installed mongoose

--- backend/src/server.js
+++ backend/src/server.js
@@ -9,3 +9,5 @@ app.use('/api/notes', notesRoutes);
 app.listen(3000, () => {
   console.log('Server is running on http://localhost:3000');
 });
+
+//mongodb+srv://mytesting2233:V7k8HPoVIlOD0ne9@cluster0.r4davnn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
