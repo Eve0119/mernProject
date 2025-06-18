58dfe7c7988fb7a74b365167d64be8601f08566a
Eve0119 <gerictorilla@gmail.com>
2025-06-18 09:07:48 +0800

    getAllNotes API

--- backend/src/controllers/notesControllers.js
+++ backend/src/controllers/notesControllers.js
@@ -1,5 +1,13 @@
-export const getAllNotes = (req, res) => {
-    res.status(200).json({ message: "You got the notes" });
+import Note from '../models/Note.js';
+
+export async function getAllNotes (req, res) {
+    try {
+        const notes = await Note.find();
+        res.status(200).json(notes);
+    } catch (error) {
+        console.error("Error in getAllNotes controller:", error);
+        res.status(500).json({ message: "Internal Server Error" });
+    }
 }
 
 export const createNote = (req, res) => {
