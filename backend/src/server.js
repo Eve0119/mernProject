9ee0a4aad9ef914080ce8a996c4c6f48f0d75e47
Eve0119 <gerictorilla@gmail.com>
2025-06-19 10:32:21 +0800

    implementing rateLimiter correctly

--- backend/src/server.js
+++ backend/src/server.js
@@ -10,11 +10,11 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.json());
-app.use(rateLimiter)
 app.use(cors({
   origin: "http://localhost:5173",
 }))
+app.use(express.json());
+app.use(rateLimiter)
 app.use((req, res, next) => {
   console.log(`${req.method} request for '${req.url}'`);
   next();
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -3,9 +3,10 @@ import Navbar from '../components/Navbar.jsx'
 import { useState, useEffect } from 'react'
 import RateLimitedUI from '../components/RateLimitedUI.jsx'
 import axios from 'axios'
+import { toast } from 'react-hot-toast'
 
 const HomePage = () => {
-  const [isRateLimited, setIsRateLimited] = useState(true);
+  const [isRateLimited, setIsRateLimited] = useState(false);
   const [notes, setNotes] = useState([]);
   const [loading, setLoading] = useState(true);
   
@@ -14,8 +15,16 @@ const HomePage = () => {
       try {
         const res = await axios.get('http://localhost:3000/api/notes');
         console.log(res.data);
+        setIsRateLimited(false);
       } catch (error) {
         console.error('Error fetching notes:', error);
+        if (error.response.status === 429) {
+          setIsRateLimited(true);
+        }else {
+          toast.error('Failed to fetch notes. Please try again later.');
+        }
+      }finally {
+        setLoading(false);
       }
     }
     fetcNotes();
