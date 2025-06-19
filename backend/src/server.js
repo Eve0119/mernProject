7f62ebf8d1e2b886a8f4b3c3b3df28017c4b1cbb
Eve0119 <gerictorilla@gmail.com>
2025-06-19 10:18:12 +0800

    included the frontend in cors so that security errors fixed

--- backend/src/server.js
+++ backend/src/server.js
@@ -12,7 +12,9 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(rateLimiter)
-app.use(cors())
+app.use(cors({
+  origin: "http://localhost:5173",
+}))
 app.use((req, res, next) => {
   console.log(`${req.method} request for '${req.url}'`);
   next();
