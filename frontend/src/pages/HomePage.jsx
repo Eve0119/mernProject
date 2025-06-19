24144fe90ab82a4c80e20b7f35779997511faa37
Eve0119 <gerictorilla@gmail.com>
2025-06-19 11:42:29 +0800

    displaying all the notes

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -4,6 +4,7 @@ import { useState, useEffect } from 'react'
 import RateLimitedUI from '../components/RateLimitedUI.jsx'
 import axios from 'axios'
 import { toast } from 'react-hot-toast'
+import NoteCard from '../components/NoteCard.jsx'
 
 const HomePage = () => {
   const [isRateLimited, setIsRateLimited] = useState(false);
@@ -15,6 +16,7 @@ const HomePage = () => {
       try {
         const res = await axios.get('http://localhost:3000/api/notes');
         console.log(res.data);
+        setNotes(res.data);
         setIsRateLimited(false);
       } catch (error) {
         console.error('Error fetching notes:', error);
@@ -34,6 +36,16 @@ const HomePage = () => {
     <div className='min-h-screen'>
       <Navbar/>
       {isRateLimited && <RateLimitedUI/>}
+      <div className='max-w-7xl mx-auto p-4 mt-6'>
+        {loading && <div className="flex justify-center items-center h-screen"><p className="text-lg">Loading...</p></div>}
+        {notes.length > 0 && !isRateLimited && (
+          <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
+            {notes.map(note => (
+              <NoteCard key={note._id} note={note} />
+            ))}
+          </div>
+        )}
+      </div>
     </div>
   )
 }
